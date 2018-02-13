bab110aaae3ce92d3ed5ecd1da3fc18e6df79445
Nick Leung <nickleung+bitbucket@gmail.com>
2018-02-12 17:23:33 -0800

    Added a player toggle.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,11 @@
 // initialize the board upon load
-// append click handlers for each space on the board
+  // initialize an array upon load to store the values
+// on click, we want to change the inner HTML
 
-var init = function () {
-  //var div = document.createElement("div");
-  // div.style.width = "100px";
-  // div.style.height = "100px";
-  // div.style.background = "red";
-  // div.style.color = "white";
-  var body = document.createElement("body");
-  var board = document.createElement("div");
+var initializeBoard = function() {
+  var board = document.createElement("table");
   board.setAttribute("id", "board");
+  board.style["border-collapse"] = "collapse";
   document.body.appendChild(board);
 
   for (var i = 0; i < 3; i++) {
@@ -19,10 +15,45 @@ var init = function () {
     for (var j = 0; j < 3; j++) {
       var col = document.createElement("td");
       col.setAttribute("id", "col:" + j);
-      col.innerHTML = "X";
+      col.style["width"] = "100px";
+      col.style["height"] = "100px";
+      col.style["background"] = "white";
+      col.style["text-align"] = "center";
+      col.style["vertical-align"] = "middle";
+      col.style["border-style"] = "solid";
+      col.innerHTML = " ";
       row.appendChild(col);
     }
   }
+}
+
+var init = function () {
+  var currentPlayer = 'X';
+
+  initializeBoard();
+
+  document.addEventListener('click', function (event) {
+    if (event.target.innerHTML === " ") {
+      event.target.innerHTML = currentPlayer;
+
+      // toggle the current player
+      currentPlayer = (currentPlayer === 'X') ? 'O' : 'X' // if i use this again, consider using a helper function
+    }
+  }, false);
+
+};
+
+var checkBoard = function () {
+  
+};
+
+var clearBoard = function () {
+  
 };
 
 init();
+
+// var switchPlayer = function (currentPlayer) {
+//   currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'
+//   return currentPlayer;
+// }
