22361079e502c656d6390837d6742fecbe9ece16
Nick Leung <nickleung+bitbucket@gmail.com>
2018-02-13 20:00:43 -0800

    Basic functionality to send text from a form to the server.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
+var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/index.html')) //
+})
+
+app.post('/login', (req, res) => {
+  console.log('req = ' + req);
+  var text = req.body.textArea;
+  console.log('text = ' + text);
+  res.end('okay');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000.'))
